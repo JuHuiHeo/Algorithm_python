59cb06cf584e6100bebe7e7b65514a7cbdee769c
Juhui Heo <mooo0502@naver.com>
2026-09-09 02:14:37 +0900

    20260909

---
+++ programmers/점프와 순간 이동.py
@@ -0,0 +1,12 @@
+def solution(n):
+    answer = 0
+    while n > 0:
+        if n % 2 == 0: # 짝수이면 //2
+            n = n // 2
+        else: # 홀수이면 -1
+            n = n - 1
+            answer += 1 # -1 하는 횟수가 답
+    
+    return answer
+
+# 문제를 너무 어렵게 생각함...
